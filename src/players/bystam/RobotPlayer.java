d301dffdf0d8d68b0473daf6dba57c3c32c41cdf
Fredrik Bystam <fredrik.bystam@bontouch.com>
2017-01-10 21:46:02 +0100

    fix junglerjack

--- src/players/bystam/RobotPlayer.java
+++ src/players/bystam/RobotPlayer.java
@@ -1,19 +1,20 @@
-package examplefuncsplayer;
+package players.bystam;
 
 import battlecode.common.*;
+
 public strictfp class RobotPlayer {
     static RobotController rc;
 
     /**
      * run() is the method that is called when a robot is instantiated in the Battlecode world.
      * If this method returns, the robot dies!
-    **/
+     **/
     @SuppressWarnings("unused")
     public static void run(RobotController rc) throws GameActionException {
 
         // This is the RobotController object. You use it to perform actions from this robot,
         // and to get information on its current status.
-        RobotPlayer.rc = rc;
+        examplefuncsplayer.RobotPlayer.rc = rc;
         // Here, we've separated the controls into a different method for each RobotType.
         // You can add the missing ones or rewrite this into your own control structure.
         switch (rc.getType()) {
@@ -33,9 +34,11 @@ public strictfp class RobotPlayer {
                 runLumberjack();
                 break;
         }
-	}
+    }
 
     static void runArchon() throws GameActionException {
+        System.out.println("I'm an archon!");
+
         // The code you want your robot to perform every round should be in this loop
         while (true) {
 
@@ -69,7 +72,8 @@ public strictfp class RobotPlayer {
         }
     }
 
-	static void runGardener() throws GameActionException {
+    static void runGardener() throws GameActionException {
+        System.out.println("I'm a gardener!");
 
         // The code you want your robot to perform every round should be in this loop
         while (true) {
@@ -106,6 +110,7 @@ public strictfp class RobotPlayer {
     }
 
     static void runSoldier() throws GameActionException {
+        System.out.println("I'm an soldier!");
         Team enemy = rc.getTeam().opponent();
 
         // The code you want your robot to perform every round should be in this loop
@@ -141,6 +146,7 @@ public strictfp class RobotPlayer {
     }
 
     static void runLumberjack() throws GameActionException {
+        System.out.println("I'm a lumberjack!");
         Team enemy = rc.getTeam().opponent();
 
         // The code you want your robot to perform every round should be in this loop
--- src/strategies/jungler/JunglerLumberjack.java
+++ src/strategies/jungler/JunglerLumberjack.java
@@ -1,7 +1,10 @@
 package strategies.jungler;
 
-import battlecode.common.RobotController;
+import battlecode.common.*;
 import common.robots.Lumberjack;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 /**
  * Created by fredrikbystam on 10/01/17.
@@ -13,7 +16,23 @@ public class JunglerLumberjack extends Lumberjack {
     }
 
     @Override
-    public void step() {
+    public void step() throws GameActionException {
+        TreeInfo[] trees = senseNearbyTrees();
 
+        Optional<TreeInfo> neutralRobotTree = getNeutralRobotTree(trees);
+
+        if (neutralRobotTree.isPresent()) {
+            TreeInfo tree = neutralRobotTree.get();
+            if (canChop(tree.getID()) && tree.location.isWithinDistance(getLocation(), getType().strideRadius)) {
+                chop(tree.getID());
+            } else if (canMove(tree.location)) {
+                move(tree.location);
+            }
+        }
+    }
+
+
+    Optional<TreeInfo> getNeutralRobotTree(TreeInfo[] trees) {
+        return Arrays.stream(trees).filter( (t) -> t.containedRobot != null ).findFirst();
     }
 }
