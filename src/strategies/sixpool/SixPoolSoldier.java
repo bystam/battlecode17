bc773064393f380cabbbbeaa0c0ee50e2ceb60b9
Fredrik Bystam <fredrik.bystam@bontouch.com>
2017-01-17 22:44:57 +0100

    soldier

--- src/strategies/sixpool/SixPoolSoldier.java
+++ src/strategies/sixpool/SixPoolSoldier.java
@@ -5,6 +5,10 @@ import common.model.MapGrid;
 import common.robots.Soldier;
 import strategies.mapping.MappingMemory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by fredrikbystam on 17/01/17.
  */
@@ -13,12 +17,14 @@ public class SixPoolSoldier extends Soldier {
     private final MappingMemory mappingMemory;
     private MapGrid grid;
 
-    private final MapLocation enemyLocation;
+    private final List<MapLocation> enemyLocations;
+    private Direction roamingDirection;
 
     public SixPoolSoldier(RobotController r) {
         super(r);
         mappingMemory = new MappingMemory(r);
-        enemyLocation = map.getInitialArchonLocations(getTeam().opponent())[0];
+        enemyLocations = new ArrayList<>(Arrays.asList(map.getInitialArchonLocations(getTeam().opponent())));
+        roamingDirection = Direction.getNorth();
     }
 
     @Override
@@ -28,7 +34,38 @@ public class SixPoolSoldier extends Soldier {
         if (fireAtAnyOf(senseNearbyRobots(-1, getTeam().opponent()))) return;
         if (fireAtAnyOf(senseNearbyTrees(-1, getTeam().opponent()))) return;
 
-        pathFindingAlgorithmMoveTowards(enemyLocation);
+        roam();
+    }
+
+    private void roam() throws GameActionException {
+        MapLocation target = getTargetLocation();
+        if (target != null) {
+            pathFindingAlgorithmMoveTowards(getTargetLocation());
+        } else {
+            if (canMove(roamingDirection)) {
+                move(roamingDirection);
+                roamingDirection = roamingDirection.rotateLeftDegrees(2);
+            } else {
+                roamingDirection = roamingDirection.rotateLeftDegrees(45);
+                if (canMove(roamingDirection)) {
+                    move(roamingDirection);
+                    roamingDirection = roamingDirection.rotateLeftDegrees(2);
+                }
+            }
+        }
+    }
+
+    private MapLocation getTargetLocation() {
+        if (enemyLocations.isEmpty()) {
+            return null;
+        }
+
+        MapLocation next = enemyLocations.get(0);
+        if (getLocation().distanceTo(next) < 3) {
+            enemyLocations.remove(0);
+            return getTargetLocation();
+        }
+        return next;
     }
 
     private boolean fireAtAnyOf(BodyInfo[] targets) throws GameActionException {
