1e145931835d6825224a141f7dcb0521a059b24d
Jens Arvidsson <jensarv@gmail.com>
2017-01-17 19:14:42 +0100

    lolf

--- src/strategies/fortress/FortressGardener.java
+++ src/strategies/fortress/FortressGardener.java
@@ -13,23 +13,39 @@ public class FortressGardener extends Gardener{
 
     private float y;
     private float GARDEN_MARGIN = 10f;
+    private boolean startedPlanting = false;
 
     public FortressGardener(RobotController r, int yIndex) {
         super(r);
-        this.y = (3-yIndex) * GARDEN_MARGIN;
+        this.y = this.getLocation().y - ((3-yIndex) * GARDEN_MARGIN);
         System.out.println("Y coord for this gardener is " + this.y);
     }
 
     @Override
     public void step() throws GameActionException {
-        if(Math.abs(getLocation().y - y) > 0.05f){
+        if(!startedPlanting && Math.abs(getLocation().y - y) > 0.05f){
             MapLocation loco = new MapLocation(getLocation().x, y);
             if(canMove(loco)){
                 move(loco);
                 return;
             }
         }
-        plantTreeInAnyDirection();
+        if(!startedPlanting && canMove(Direction.getWest())){
+            move(Direction.getWest());
+            return;
+        }
+        startedPlanting = true;
+
+        //now at westmost point. start planting trees until some x
+        if(canPlantTree(Direction.getSouth())){
+            plantTree(Direction.getSouth());
+            return;
+        }
+
+        if(hasTreeBuildRequirements() && canMove(Direction.getEast())){
+            move(Direction.getEast());
+        }
+
     }
 
     private boolean plantTreeInAnyDirection() throws GameActionException{
